01ab9517e7b8519129f4cf364453bd04dba327b0
fatorarpolinomio <xextreme225@gmail.com>
2026-04-07 20:36:02 -0300

    Brincando com servidores

--- cmd/dragonite/main.go
+++ cmd/dragonite/main.go
@@ -1,7 +1,60 @@
-package dragonite
+package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
+
+	"golang.org/x/net/websocket"
+)
+
+type Server struct {
+	conns map[*websocket.Conn]bool
+}
+
+func NewServer() *Server {
+	return &Server{
+		conns: make(map[*websocket.Conn]bool),
+	}
+}
+
+func (s *Server) readLoop(ws *websocket.Conn) {
+	buf := make([]byte, 1024)
+
+	for {
+		n, err := ws.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			fmt.Println("read error:", err)
+			continue
+		}
+		msg := buf[:n]
+		s.broadcast(msg)
+	}
+}
+
+func (s *Server) handleWS(ws *websocket.Conn) {
+	fmt.Println("New incoming connection from client:", ws.RemoteAddr())
+
+	s.conns[ws] = true
+
+	s.readLoop(ws)
+}
+
+func (s *Server) broadcast(msg []byte) {
+	for ws := range s.conns {
+		go func(ws *websocket.Conn) {
+			if _, err := ws.Write(msg); err != nil {
+				fmt.Println("write error:", err)
+			}
+		}(ws)
+	}
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	server := NewServer()
+	http.Handle("/ws", websocket.Handler(server.handleWS))
+	http.ListenAndServe(":8080", nil)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/caio-bernardo/dragonite
 
 go 1.26.1
+
+require golang.org/x/net v0.52.0 // indirect
