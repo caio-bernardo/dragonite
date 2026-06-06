212a69858d70eedc43b123a8c816cbcd53683f75
Caio Bernardo <me@caiobernardo.top>
2026-06-06 09:16:13 -0300

    feat(client/profile): atualizado com sucesso

--- internal/delivery/http_adapter/client/profile/routes.go
+++ internal/delivery/http_adapter/client/profile/routes.go
@@ -4,16 +4,17 @@ import (
 	"net/http"
 
 	"github.com/caio-bernardo/dragonite/internal/delivery/http_adapter/httputil"
+	"github.com/caio-bernardo/dragonite/internal/domain"
 	"github.com/caio-bernardo/dragonite/internal/domain/types"
 	"github.com/caio-bernardo/dragonite/internal/usecase"
 )
 
 type Handler struct {
-	userService usecase.UsuarioService
+	profileService usecase.ProfileService
 }
 
-func NewHandler(userService usecase.UsuarioService) *Handler {
-	return &Handler{userService: userService}
+func NewHandler(profileService usecase.ProfileService) *Handler {
+	return &Handler{profileService: profileService}
 }
 
 func (h *Handler) RegisterRoutes(mux *http.ServeMux, authMiddleware types.Middleware) {
@@ -33,7 +34,7 @@ func (h *Handler) RegisterRoutes(mux *http.ServeMux, authMiddleware types.Middle
 func (h *Handler) getProfile(w http.ResponseWriter, r *http.Request) {
 	userID := r.PathValue("userId")
 
-	profile, err := h.userService.GetProfileByID(r.Context(), userID)
+	profile, err := h.profileService.GetProfileByUserID(r.Context(), userID)
 
 	if err != nil {
 		if err == types.ErrNotFound {
@@ -58,7 +59,7 @@ func (h *Handler) getProfileKey(w http.ResponseWriter, r *http.Request) {
 	userID := r.PathValue("userId")
 	keyName := r.PathValue("keyName") // Vai ser "displayname" ou "avatar_url"
 
-	usuario, err := h.userService.GetProfileByID(r.Context(), userID)
+	usuario, err := h.profileService.GetProfileByUserID(r.Context(), userID)
 
 	if err != nil {
 		if err == types.ErrNotFound {
@@ -75,11 +76,11 @@ func (h *Handler) getProfileKey(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	var valor string
+	var valor *string
 	if keyName == "displayname" {
-		valor = *usuario.DisplayName
+		valor = usuario.DisplayName
 	} else if keyName == "avatar_url" {
-		valor = *usuario.AvatarURL
+		valor = usuario.AvatarURL
 	} else {
 		// Se pediu uma chave que não existe no Matrix
 		httputil.WriteMatrixError(w, http.StatusBadRequest,
@@ -89,7 +90,7 @@ func (h *Handler) getProfileKey(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	response := map[string]string{
+	response := map[string]*string{
 		keyName: valor,
 	}
 
@@ -100,15 +101,6 @@ func (h *Handler) getProfileKey(w http.ResponseWriter, r *http.Request) {
 func (h *Handler) putProfileKey(w http.ResponseWriter, r *http.Request) {
 	userID := r.PathValue("userId")
 	keyName := r.PathValue("keyName") // "displayname" ou "avatar_url"
-
-	colunaDB := mapMatrixKeyToDB(keyName)
-	if colunaDB == "" {
-		httputil.WriteMatrixError(w, http.StatusBadRequest,
-			"M_BAD_JSON",
-			"Invalid profile key",
-		)
-		return
-	}
 
 	var reqBody map[string]string
 	if err := httputil.ParseBody(r, &reqBody); err != nil {
@@ -128,7 +120,20 @@ func (h *Handler) putProfileKey(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	err := h.userService.UpdateProfileKey(r.Context(), userID, colunaDB, novoValor)
+	props := domain.Profile{
+		IDUsuario:   userID,
+		DisplayName: nil,
+		AvatarURL:   nil,
+	}
+
+	switch keyName {
+	case "displayname":
+		props.DisplayName = &novoValor
+	case "avatar_url":
+		props.AvatarURL = &novoValor
+	}
+
+	err := h.profileService.UpdateProfile(r.Context(), props)
 	if err != nil {
 		if err == types.ErrNotFound {
 			httputil.WriteMatrixError(w, http.StatusNotFound,
@@ -153,16 +158,20 @@ func (h *Handler) deleteProfileKey(w http.ResponseWriter, r *http.Request) {
 	userID := r.PathValue("userId")
 	keyName := r.PathValue("keyName")
 
-	colunaDB := mapMatrixKeyToDB(keyName)
-	if colunaDB == "" {
-		httputil.WriteMatrixError(w, http.StatusBadRequest,
-			"M_BAD_JSON",
-			"Invalid profile key",
-		)
-		return
+	props := domain.Profile{
+		IDUsuario:   userID,
+		DisplayName: nil,
+		AvatarURL:   nil,
 	}
 
-	err := h.userService.ClearProfileKey(r.Context(), userID, colunaDB)
+	switch keyName {
+	case "displayname":
+		props.DisplayName = new("")
+	case "avatar_url":
+		props.AvatarURL = new("")
+	}
+
+	err := h.profileService.UpdateProfile(r.Context(), props)
 
 	if err != nil {
 		httputil.WriteMatrixError(w, http.StatusInternalServerError,
@@ -173,17 +182,4 @@ func (h *Handler) deleteProfileKey(w http.ResponseWriter, r *http.Request) {
 	}
 
 	httputil.WriteJSON(w, http.StatusOK, map[string]string{})
-}
-
-// mapMatrixKeyToDB converte a chave do Matrix para a coluna do bd.
-// Isso evita SQL Injection, pois não usamos a string do usuário direto na query.
-func mapMatrixKeyToDB(keyName string) string {
-	switch keyName {
-	case "displayname":
-		return "nome_usuario"
-	case "avatar_url":
-		return "foto_usuario"
-	default:
-		return ""
-	}
 }
